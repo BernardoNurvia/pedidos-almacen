12a666ba04d6bfff583ef0e1982bbd9676f630ef
Bernardo Patiño <bap_link@hotmail.com>
2024-10-26 22:06:49 +0000

    primera toma de contacto con backend para crear los models

--- src/api/models.py
+++ src/api/models.py
@@ -5,8 +5,8 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(150), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(True), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -17,3 +17,8 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Material(db.Model):
+    id=db.Colum(db.Interger, primary_key=True)
+    name = db.Colum(db.String(100), unique=True)
+    
