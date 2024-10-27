a1e1e29c7972887967a90fb6c1d865c6e9a02ce2
Bernardo Alexis Patiño Chávez <167910567+BernardoNurvia@users.noreply.github.com>
2024-10-27 14:22:05 +0000

    css en linea para Login

--- src/front/js/pages/Login.js
+++ src/front/js/pages/Login.js
@@ -1,27 +1,27 @@
 import React from 'react'
 
 import logo from './../../img/logo.png'
-import background from './../../img/background.plugin'
+
 
 
 const Login = () => {
   return (
     <div className='container'>
-        <div className='text-center' style={{backgroundImage: src={background}}}>
+        <div className='text-center m-auto'style={{width:"20rem", backgroundColor:"purple", borderRadius:"5px"}} >
             <h1 className=''>Solicitud de Material</h1>
-            <img src={logo} style={{width:"8rem"}} alt="1KOMMA5º" />
+            <img src={logo} style={{width:"10rem"}} alt="1KOMMA5º" />
             <br/>
-            <h2>Usuario</h2>
-            <br/>
+            <h2 className='mt-1'>Usuario</h2>
+           
             <input type="email" />
-            <br/>
-            <h2>Contraseña</h2>
-            <br/>
+            
+            <h2 className='mt-1'>Contraseña</h2>
+            
             <input type="password" />
             <br/>
-            <button className='btn'>Login</button>
+            <button className='btn btn-success mt-1'>Login</button>
             <br/>
-            <h4>Registrate<a href="">Aqui</a></h4>
+            <h5 className='mt-1'>Registrate<a href="">Aqui</a></h5>
         </div>
 
     </div>
